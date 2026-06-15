4432f144ef7ee92652d60076ba24194dee0e25a8
Shakunthala15997 <160107334+Shakunthala15997@users.noreply.github.com>
2026-06-15 15:01:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("Testing pull command");
 console.log("Hello from Shakunthala project")
-console.log("Login feature added")
+console.log("Login feature added to jenkin demo")
 
 login features add
 features1 added
